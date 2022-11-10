dfd86cd799ae4ec01d380f743ea587b995b4dbe2
Nate Traugh <n.traugh@yahoo.com>
2022-11-10 15:23:18 -0500

    mapped over our posts data to display the cards, added update and delete buttons for each note, need to create those routes

--- client/src/Posts.js
+++ client/src/Posts.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { Button } from 'react-bootstrap'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 
 
 
 const Posts = () => {
+    const [posts, setPosts] = useState([])
+    const navigate = useNavigate()
+    
     useEffect(() => {
         axios.get("/posts")
-            .then(res => console.log(res))
+            .then(res => {
+                console.log(res)
+                setPosts(res.data)
+            })
             .catch(err => console.log(err))
     }, [])
     
@@ -16,6 +24,27 @@ const Posts = () => {
   return (
     <div style={{width: "90%", textAlign: "center", margin: "auto auto"}}>
         <h1>Posts</h1>
+        {posts ? (
+            <>
+                {posts.map((post) => {
+                    return (
+                        <div style={{border: "solid black 1px", borderRadius: "4px", marginBottom: "1rem", padding: "1rem"}}>
+                            <h4>{post.title}</h4>
+                            <p>{post.description}</p>
+                            <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                                <Button variant="outline-primary" style={{width: "100%", marginRight: "1rem"}}>Update</Button>
+                                <Button variant="outline-danger" style={{width: "100%", marginRight: "1rem"}}>Delete</Button>
+                            </div>
+                        </div>
+                    )
+                })}
+            
+            </>
+        ) : ""}
+        <Button 
+        style={{width: "100%", marginTop: "1rem"}}
+        variant="outline-primary" 
+        onClick={() => navigate("posts")}>Back</Button>
     </div>
   )
 }
