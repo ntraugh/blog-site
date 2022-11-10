448c72c7e569683d9fc4366e3dded9cda8b057b7
Nate Traugh <n.traugh@yahoo.com>
2022-11-10 14:51:20 -0500

    set up the mongoose schema and our mongoose model that we can now use Create on to post our data to the database

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 const mongoose = require("mongoose")
-// small change to see node modules
+
 
 
 app.use(express.json())
 
+mongoose.connect("mongodb://localhost:27017/nateblogdb")
+    .catch(err => console.log(err))
+
+const postSchema = mongoose.Schema({
+    title: String,
+    description: String
+})
+
+const Post = mongoose.model("Post", postSchema)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
